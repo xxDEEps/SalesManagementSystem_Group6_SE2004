27be33d54ec117fdd0d18a0900fb25e4fad9e305
xxDEEps <quangdang0704@gmail.com>
2026-06-19 14:56:24 +0700

    Update ProductController.java

--- src/Controller/ProductController.java
+++ src/Controller/ProductController.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import java.util.Collections;
+import java.util.List;
+
+import Model.Product;
 import Services.ProductService;
 
 public class ProductController {
@@ -7,5 +11,60 @@ public class ProductController {
 
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    public String handleAddNewProduct(Product product) {
+        try {
+            boolean success = productService.addNewProduct(product);
+            if (success) {
+                return "Product added successfully.";
+            } else {
+                return "Failed to add product. Product with the same ID already exists.";
+            }
+        } catch (Exception e) {
+            return "Error occurred while saving product to file: " + e.getMessage();
+        }
+    }
+
+    public String handleUpdateProduct(String id, Product product) {
+        try {
+            boolean success = productService.updateProduct(id, product);
+            if (success) {
+                return "Product updated successfully.";
+            } else {
+                return "Failed to update product. Product may not exist or has been deleted.";
+            }
+        } catch (Exception e) {
+            return "Error occurred while updating product in file: " + e.getMessage();
+        }
+    }
+
+    public String handleDeleteProduct(String id) {
+        try {
+            boolean success = productService.deleteProduct(id);
+            if (success) {
+                return "Product deleted successfully.";
+            } else {
+                return "Failed to delete product. Product may not exist or has been deleted.";
+            }
+        } catch (Exception e) {
+            return "Error occurred while deleting product from file: " + e.getMessage();
+        }
+    }
+
+    public String handleGetAllProducts() {
+        try {
+            return productService.getAllProducts().toString();
+        } catch (Exception e) {
+            return "Error occurred while retrieving products from file: " + e.getMessage();
+        }
+    }
+
+    public List<Product> handleSearchProductsByNameOrCategory(String keyword) {
+        try {
+            return productService.searchProductsByNameOrCategory(keyword);
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
     }
 }
